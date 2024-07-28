cefcdccfc9926735ce0f2c1d87c16b86298d12ab
BgNikoto11 <148450284+BgNikoto11@users.noreply.github.com>
2024-07-28 16:25:02 +0300

    integrated navbar component

--- front_end/components/navbar-component.php
+++ front_end/components/navbar-component.php
@@ -1,11 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>nav</p>
-</body>
-</html>
+<?php
+    session_start();
+    if(isset($_SESSION['username'])){       
+        function displayStatus(){
+            global $username;
+            $username = $_SESSION['username'];
+
+            echo"<h3>Welcome, $username!</h3>";
+            echo"<button class='logout-button'>Logout</button>";
+        }      
+    }
+    else{
+       function displayStatus(){
+        echo"<button class='login-button'>Login</button>";
+        echo"<button class='register-button'>Signup</button>";
+       }
+    }
+?>
+<nav>
+    <div class="logo-contaier">
+        <h1>Literature Hub</h1>
+    </div>
+    <div class="navigation-links"></div>
+    <div class="user-status">
+        <?php displayStatus()?>
+    </div>
+</nav>
---
+++ front_end/script/redirect.js
@@ -0,0 +1,19 @@
+var loginButton = document.querySelector('.login-button');
+var logoutButton = document.querySelector(".logout-button");
+var registerButton = document.querySelector(".register-button");
+
+if (loginButton != null) {
+    loginButton.addEventListener("click", function () {
+        window.location.href = "/literature_hub/front_end/markup/login.php";
+    });
+}
+if (registerButton != null) {
+    registerButton.addEventListener("click", function () {
+        window.location.href = "/literature_hub/front_end/markup/register.php";
+    });
+}
+if (logoutButton != null) {
+    logoutButton.addEventListener("click", function () {
+        window.location.href = "/literature_hub/back_end/logout-handler.inc.php";
+    });
+}
