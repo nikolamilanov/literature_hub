55320a42ece7f3dd3793bda92134561a43c86b59
BgNikoto11 <148450284+BgNikoto11@users.noreply.github.com>
2024-08-06 16:17:28 +0300

    improved style

--- front_end/markup/index.php
+++ front_end/markup/index.php
@@ -1,33 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
-    
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="../style/navbar.css">
+    <link rel="stylesheet" href="../style/table.css">
     <link rel="stylesheet" href="../style/reset.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <script src="../script/redirect.js" defer></script>
     <script src="../script/fetch-data.js" defer></script>
 </head>
 
 <body>
     <?php require_once "../components/navbar-component.php" ?>
+    <div class="table-wrapper">
+        <form id="filter-form">
 
-    <form id="filter">
+            <div class="filter-wrapper">
+                <label for="filter-type">Filter by:</label>
+                <select name="filter-type" id="filter-type">
+                    <option value="genre">Genre</option>
+                    <option value="writer">Writer</option>
+                    <option value="creation">Creation</option>
+                </select>
+            </div>
 
-        <label for="filter-type">Filter by:</label>
-        <select name="filter-type" id="filter-type">
-            <option value="genre">Genre</option>
-            <option value="writer">Writer</option>
-            <option value="creation">Creation</option>
-        </select>
+            <input type="text" name="filter-value" id="filter-value" placeholder="Search">
+            <button type=submit><i class="bi bi-search"></i></button>
+        </form>
 
-        <input type="text" name="filter-value" id="filter-value" placeholder="Search">
-        <input type="submit">
-    </form>
-
-    <div id="table-container"></div>
+        <div id="table-container"></div>
+    </div>
 
 </body>
 
