866922de65a96a1d780f5408a25ecadfcf8abfda
BgNikoto11 <148450284+BgNikoto11@users.noreply.github.com>
2024-08-20 17:17:49 +0300

    updated database

--- db.sql
+++ db.sql
@@ -1,5 +1,5 @@
 -- Users table
-CREATE TABLE Users (
+CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     user_name CHAR(30) NOT NULL,
     user_email CHAR(50) NOT NULL,
@@ -8,59 +8,57 @@ CREATE TABLE Users (
 );
 
 -- ChangesRequests table
-CREATE TABLE ChangesRequests (
+CREATE TABLE changes_requests (
     request_id INT AUTO_INCREMENT PRIMARY KEY,
     requested_by INT NOT NULL,
     request_timestamp DATETIME NOT NULL,
     request_status ENUM('pending', 'approved', 'rejected') NOT NULL,
-    FOREIGN KEY (requested_by) REFERENCES Users(user_id)
-);
-
--- ChangesLogs table
-CREATE TABLE ChangesLogs (
-    log_id INT AUTO_INCREMENT PRIMARY KEY,
-    log_timestamp DATETIME NOT NULL,
-    changed_by INT NOT NULL,
-    approved_by INT NULL,
-    action_type ENUM('create', 'update', 'delete') NOT NULL,
-    FOREIGN KEY (changed_by) REFERENCES Users(user_id),
-    FOREIGN KEY (approved_by) REFERENCES Users(user_id)
+    FOREIGN KEY (requested_by) REFERENCES users(user_id)
 );
 
 -- Writers table
-CREATE TABLE Writers (
+CREATE TABLE writers (
     writer_id INT AUTO_INCREMENT PRIMARY KEY,
-    writer_name CHAR(30) NOT NULL,
-    writer_surname CHAR(30) NOT NULL,
-    writer_nickname CHAR(50),
+    writer_name CHAR(60) NOT NULL,
     birth_date DATE,
     death_date DATE,
     birth_place CHAR(50)
 );
 
 -- Creations table
-CREATE TABLE Creations (
+CREATE TABLE creations (
     creation_id INT AUTO_INCREMENT PRIMARY KEY,
+    creation_name CHAR(60) NOT NULL,
     creation_genre CHAR(20) NOT NULL,
     creation_writer INT NOT NULL,
     creation_date DATE NOT NULL,
-    FOREIGN KEY (creation_writer) REFERENCES Writers(writer_id)
+    FOREIGN KEY (creation_writer) REFERENCES writers(writer_id)
 );
 
+-- ChangesLogs table
+CREATE TABLE changes_logs (
+    log_id INT AUTO_INCREMENT PRIMARY KEY,
+    log_timestamp DATETIME NOT NULL,
+    changed_by INT NOT NULL,
+    approved_by INT NULL,
+    action_type ENUM('create', 'update', 'delete') NOT NULL,
+    FOREIGN KEY (changed_by) REFERENCES users(user_id),
+    FOREIGN KEY (approved_by) REFERENCES users(user_id)
+);
 -- WritersChangesList table
-CREATE TABLE WritersChangesList (
+CREATE TABLE writers_changes_list (
     list_id INT AUTO_INCREMENT PRIMARY KEY,
     record_id INT NOT NULL,
     log_id INT NOT NULL,
-    FOREIGN KEY (log_id) REFERENCES ChangesLogs(log_id),
-    FOREIGN KEY (record_id) REFERENCES Writers(writer_id)
+    FOREIGN KEY (log_id) REFERENCES changes_logs(log_id),
+    FOREIGN KEY (record_id) REFERENCES writers(writer_id)
 );
 
 -- CreationsChangesList table
-CREATE TABLE CreationsChangesList (
+CREATE TABLE creations_changes_list (
     list_id INT AUTO_INCREMENT PRIMARY KEY,
     record_id INT NOT NULL,
     log_id INT NOT NULL,
-    FOREIGN KEY (log_id) REFERENCES ChangesLogs(log_id),
-    FOREIGN KEY (record_id) REFERENCES Creations(creation_id)
+    FOREIGN KEY (log_id) REFERENCES changes_logs(log_id),
+    FOREIGN KEY (record_id) REFERENCES creations(creation_id)
 );
